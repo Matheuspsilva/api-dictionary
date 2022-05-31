0755b9a3adb897ebfb10bafd89dbec0c34779ee2
Matheus <matheuspsilva29@gmail.com>
2022-05-31 11:09:48 -0300

    Commit inicial

---
+++ src/main/java/com/matheussilvadev/ApiDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.matheussilvadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDictionaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheussilvadev/ApiDictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheussilvadev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
